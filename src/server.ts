395a3e4ec56f3889a77ec3d1e2b351cae1e19e92
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-01 10:52:51 +0200

    fixing errors related to importing the loggermiddleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-const logger = require("./middleware/middlewareLogger");
+import { logger } from './middlewares/middlewareLogger';
 
 const app = express();
 const PORT = 3000;
