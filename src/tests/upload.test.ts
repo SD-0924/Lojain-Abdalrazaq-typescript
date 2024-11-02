335279fc2496e82de1467c9de96db277313a6709
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-02 22:03:13 +0200

    upload image unit test

---
+++ src/tests/upload.test.ts
@@ -0,0 +1,39 @@
+import request from 'supertest';
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+import { uploadImage } from '../controllers/imageController';
+
+const app = express();
+const upload = multer();
+
+// upload image endpoint
+app.post('/api/upload', upload.single('image'), uploadImage);
+
+describe('Upload Image Endpoint', () => {
+
+    // Test case 1: Successfully upload an image
+    it('should upload an image successfully', async () => {
+
+        const imagePath = path.resolve(__dirname, 'images', 'test.jpg');
+        
+        const response = await request(app)
+        .post('/api/upload')
+        .attach('image', imagePath);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('message', 'File uploaded successfully!');
+    });
+
+    // Test case 2: Return an error if no image is attached
+    it('should return an error if no image is uploaded', async () => {
+
+        // Send a POST request to the /api/upload endpoint without attaching an image
+        const response = await request(app)
+            .post('/api/upload');
+
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('message', 'No file uploaded. Please upload an image file.');
+    });
+});
+
