3a7d7d0e208c759b5d69d5c820a3c73dc9bc172e
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-03 00:39:47 +0200

    final changes on api implementation and unit test

--- tests/resize.test.ts
+++ tests/resize.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import express from 'express';
-import { resizeImage } from '../controllers/imageController';
+import { resizeImage } from '../src/controllers/imageController';
 
 const app = express();
 app.use(express.json());
--- tests/upload.test.ts
+++ tests/upload.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import express from 'express';
 import multer from 'multer';
 import path from 'path';
-import { uploadImage } from '../controllers/imageController';
+import { uploadImage } from '../src/controllers/imageController';
 
 const app = express();
 const upload = multer();
