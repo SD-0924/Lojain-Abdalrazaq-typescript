37f5e78606842fbb706b84e48ec6f6d4f935f686
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-11-02 20:54:16 +0200

    all endpoints are impleemnted until now

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -3,11 +3,8 @@ import { logError } from '../middlewares/middlewareLogger';
 
 const handleError = (req: Request, res: Response, message: string, statusCode: number) => {
 
-    // use the logError middleware to log the error
     logError(message);
-    // Send the error response to the user as JSON
     return res.status(statusCode).json({ success: false, message });
 };
 
-// export the function
 export { handleError };
